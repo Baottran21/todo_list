d42fb9aad61665257ea764abe55621fe7cc3b7ac
Bao Tran <Baottran21@outlook.com>
2023-06-25 23:20:40 -0500

    Testing Functionality on website

--- public/app.js
+++ public/app.js
@@ -1,1 +1,16 @@
-console.log('Database is linked to webpage');
+const url = `https://buildatodo.onrender.com`;
+
+window.addEventListener('load', () => {
+  getAll();
+});
+
+//GET ALL
+const getAll = async () => {
+  try {
+    const response = await fetch(`$url/todos`);
+    const data = respons.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
