734347c14b97e5b4414a7f633b5dcbe8ddfb888e
Bao Tran <Baottran21@outlook.com>
2023-06-25 23:34:16 -0500

    Working on the get All on window load

--- public/app.js
+++ public/app.js
@@ -1,16 +1,20 @@
 const url = `https://buildatodo.onrender.com`;
 
-window.addEventListener('load', async () => {
-  await getAll();
+window.addEventListener('load', () => {
+  getAll();
 });
 
 //GET ALL
-const getAll = async () => {
+async function getAll() {
   try {
-    const response = await fetch(`$url/todos`);
+    const response = await fetch(`$url/todos`, {
+      method: 'GET',
+      headers: { 'Content-Type': 'application/json' },
+      mode: 'no-cors',
+    });
     const data = await response.json();
     console.log(data);
   } catch (error) {
     console.log(error);
   }
-};
+}
